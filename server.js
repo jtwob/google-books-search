9cc1a28622aad22f476158beddde53ec569da96f
jtwob <32282322+jtwob@users.noreply.github.com>
2021-03-11 17:28:02 -0800

    UPDATE: please fix the bugs

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 mongoose.connect(
-  process.env.MONGO_URI || "mongodb://localhost/googlebooks",
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
